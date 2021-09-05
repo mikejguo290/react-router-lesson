babbd6990f5b002f1f5c23e15dc12454c08abf66
mikejguo290 <mikejguo290@gmail.com>
2021-09-05 14:17:30 +0100

    import useHistory to implement back and foward buttons' with history.goBack(), history.goForward().

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { useHistory } from 'react-router-dom';
 
 export default function Footer() {
 
   // Get the history object
+  const history = useHistory(); // useHistory to imperatively update browser location without rendering redirect. 
 
   const goBack = () => {
     // imperatively redirect back
+    history.goBack();
 
   }
 
   const goForward = () => {
     // imperatively redirect forward
-    
+    history.goForward();
   }
 
   return (
