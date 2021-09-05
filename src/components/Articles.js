8099029ddc49fac71b298c34d860434d273c2cd0
mikejguo290 <mikejguo290@gmail.com>
2021-09-05 15:11:13 +0100

    get query param values from url with useLocation.search &  new URLSearchParams object to render articles in response to search queries.

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -4,17 +4,23 @@ import { selectArticles, filterArticles } from "../features/articles/articlesSli
 import Search from "./Search";
 
 // Import Link from React Router
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Articles () {
   const articles = useSelector(selectArticles)
   
   // grab the search value from useLocation()
+  const { search } = useLocation(); // returns search string
   
   // get the queryParams from new URLSearchParams() 
+  const queryParams = new URLSearchParams(search) // pass search string to object constructor to receive dictionary of param:param value.
   
-  const title = '' // <-- fix me!
-  
+  const title = queryParams.get('title'); // <-- fix me!
+
+  // for reference const filterArticles = (query, articles) => Object.values(articles).filter(article => article.title.toLowerCase().includes(query.toLowerCase()))
+  // allArticles.filter(a => a.title.toLowerCase().includes(query.toLowerCase));
+  // Object.values(obj) returns an array of object's ennumerable property values. Articles used to create articlesSlice's initial value is an Object.
+  // so selectArticles returns an object as well. 
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
 
   return (
